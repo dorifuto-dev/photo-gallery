afc4dacd960cae3a1b49c7e3e0c57c1efecaf329
Eric Li <75854628+dorifuto-dev@users.noreply.github.com>
2024-10-16 19:44:41 -0600

    Add functionality to take and display photos in a grid

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -6,6 +6,13 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
         <ion-fab-button @click="takePhoto()">
           <ion-icon :icon="camera"></ion-icon>
@@ -31,4 +38,7 @@ import {
   IonCol,
   IonImg,
 } from '@ionic/vue';
+import {usePhotoGallery, UserPhoto} from "@/composables/usePhotoGallery";
+
+const { photos, takePhoto } = usePhotoGallery();
 </script>
